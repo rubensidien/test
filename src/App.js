517577dbe0180b74a1a0822b22717dc53659e906
rubensidien <yvan1510@hotmail.com>
2021-03-22 21:22:58 +0400

    22march
    22 march

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import HelloWorld from './Components/HelloWorld'
+import CounterExample from './Components/CounterExample'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         HEllo I am the best Full Stack developer in the world 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   
+  {/*<CounterExample />*/}
+  
+  <Header />
+   <HelloWorld name ="Yvan Ruben" />
+   <Footer />
+      
     </div>
   );
 }
---
+++ src/Components/CounterExample.js
@@ -0,0 +1,27 @@
+import React, {useState } from 'react'
+
+
+function CounterExample(){
+	const [count, setCount] = useState(0)
+
+
+	return(
+		<div>
+
+		<h1>
+		{count}
+		</h1>
+
+		<h1 onClick={() => setCount(count + 1)}>
+		Plus
+		</h1>
+		<h1 onClick={() => setCount(count -2)}>
+		Minus
+		</h1>
+
+		</div>
+
+		)
+
+} 
+export default CounterExample

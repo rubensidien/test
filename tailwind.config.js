6d827d727b1100cbc470020e042050c1e60a23cb
rubensidien <yvan1510@hotmail.com>
2021-03-28 23:02:49 +0400

    menu transition part1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+    	backgroundColor:{ 'black-t-50':'rgba(0,0,0,0.5)',
+    	}
+    },
   },
   variants: {
     extend: {},
